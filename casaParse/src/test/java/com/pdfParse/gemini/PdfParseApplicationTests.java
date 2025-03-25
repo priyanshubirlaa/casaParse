dc237917f00fce0d17fba6690cdd8818bab20f65
priyanshubirlaa <priyanshubirla02@gmail.com>
2025-03-25 11:33:14 +0530

    Push casaParse Project

---
+++ casaParse/src/test/java/com/pdfParse/gemini/PdfParseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfParse.gemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfParseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
